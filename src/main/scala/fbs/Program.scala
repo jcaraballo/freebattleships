e5d2df0a661d64754a08321941c79a3e4b6dece1
jcaraballo <git@caraballo.eu>
2016-11-20 02:01:57 +0000

    Clean up

--- src/main/scala/fbs/Program.scala
+++ src/main/scala/fbs/Program.scala
@@ -4,15 +4,6 @@ import fbs.Freeing.{CommandF, place, shoot}
 
 object Program {
 
-  val commands = List(
-    Place(Ship(Position(1, 1), Position(5, 1))),
-    Place(Ship(Position(3, 3), Position(6, 3))),
-    Place(Ship(Position(9, 1), Position(9, 5))),
-    Shoot(Position(1, 1)),
-    Shoot(Position(1, 2))
-  )
-
-
   def program: CommandF[Boolean] =
     for {
       _ <- place(Ship(Position(1, 1), Position(5, 1)))
